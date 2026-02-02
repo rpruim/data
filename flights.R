44ed3c6f69e975598b8ffbb252d84d6240098208
Randall Pruim <rpruim@gmail.com>
2026-02-02 00:40:10 -0500

    fix filename in script

---
+++ flights.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+library(nycflights13)
+library(mosaic)
+
+some_flights <-
+  flights |>
+  filter(
+    dest %in% c("LAX", "SFO", "BQN"),
+    carrier %in% c("B6", "UA")
+  ) |>
+  mutate(
+    status = if_else(arr_delay <= 0, "on time", "delayed")
+  ) |>
+  drop_na() |>
+  select(carrier, destination = dest, status)
+
+write_csv(some_flights, "~/Courses/data/docs/some-flights.csv")
+
+df_stats(
+  status ~ carrier + destination,
+  data = some_flights,
+  props,
+  format = "wide"
+) # |> pull(prop_delayed)
+
+
+df_stats(
+  status ~ carrier,
+  data = some_flights,
+  props,
+  format = "wide"
+) |>
+  pull(prop_delayed)
